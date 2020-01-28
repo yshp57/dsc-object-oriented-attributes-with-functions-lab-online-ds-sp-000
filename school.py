4662dde27a30866cf1fce8cc2ee59c6bafc98a93
JosephSzpigiel <yshp57@gmail.com>
2020-01-27 21:52:36 -0500

    lab complete

--- school.py
+++ school.py
@@ -1,1 +1,18 @@
 class School:
+    def __init__(self, name=None, roster={}):
+        self.name = name
+        self.roster = roster
+        
+    def add_student(self, fullname, grade):
+        if grade not in self.roster.keys():
+            self.roster[grade] = [fullname]
+        else:
+            self.roster[grade].append(fullname)
+            
+    def grade(self, grade):
+        return self.roster[grade]
+    
+    def sort_roster(self):
+        for grade in self.roster:
+            self.roster[grade] = sorted(self.roster[grade])
+        return self.roster
